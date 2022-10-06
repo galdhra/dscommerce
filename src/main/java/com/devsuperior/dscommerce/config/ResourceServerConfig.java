9b4f66cf7ece960d5522c26ba3731c8dab126624
luiz <lufeaz@DESKTOP-9UOBV72.localdomain>
2022-10-06 14:58:34 -0300

    cors

--- src/main/java/com/devsuperior/dscommerce/config/ResourceServerConfig.java
+++ src/main/java/com/devsuperior/dscommerce/config/ResourceServerConfig.java
@@ -1,13 +1,17 @@
 package com.devsuperior.dscommerce.config;
 
 import org.springframework.beans.factory.annotation.*;
-import org.springframework.context.annotation.Configuration;
+import org.springframework.boot.web.servlet.*;
+import org.springframework.context.annotation.*;
+import org.springframework.core.*;
 import org.springframework.security.config.annotation.method.configuration.*;
 import org.springframework.security.config.annotation.web.builders.*;
 import org.springframework.security.oauth2.config.annotation.web.configuration.*;
 import org.springframework.security.oauth2.config.annotation.web.configurers.*;
 import org.springframework.security.oauth2.provider.token.store.*;
 import org.springframework.core.env.Environment;
+import org.springframework.web.cors.*;
+import org.springframework.web.filter.*;
 
 
 import java.util.*;
@@ -17,6 +21,8 @@ import java.util.*;
 @EnableGlobalMethodSecurity(prePostEnabled = true)
 public class ResourceServerConfig extends ResourceServerConfigurerAdapter {
 
+    @Value("${cors.origins}")
+    private String corsOrigins;
     @Autowired
     private Environment env;
 
@@ -37,6 +43,32 @@ public class ResourceServerConfig extends ResourceServerConfigurerAdapter {
         }
 
         http.authorizeRequests().anyRequest().permitAll();
+
+        http.cors().configurationSource(corsConfigurationSource());
+    }
+
+    @Bean
+    CorsConfigurationSource corsConfigurationSource() {
+
+        String[] origins = corsOrigins.split(",");
+
+        CorsConfiguration corsConfig = new CorsConfiguration();
+        corsConfig.setAllowedOriginPatterns(Arrays.asList(origins));
+        corsConfig.setAllowedMethods(Arrays.asList("POST", "GET", "PUT", "DELETE", "PATCH"));
+        corsConfig.setAllowCredentials(true);
+        corsConfig.setAllowedHeaders(Arrays.asList("Authorization", "Content-Type"));
+
+        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+        source.registerCorsConfiguration("/**", corsConfig);
+        return source;
+    }
+
+    @Bean
+    FilterRegistrationBean<CorsFilter> corsFilter() {
+        FilterRegistrationBean<CorsFilter> bean
+                = new FilterRegistrationBean<>(new CorsFilter(corsConfigurationSource()));
+        bean.setOrder(Ordered.HIGHEST_PRECEDENCE);
+        return bean;
     }
 
 
